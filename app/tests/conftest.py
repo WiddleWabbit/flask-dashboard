3330b6591b5e1467597dda7c305d6e8f8dd9193a
Nathan Wheat <nwheat@westnet.com.au>
2025-05-31 17:17:02 +0800

    Optimised running of tests slightly with additional functions and fixture.

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,27 +3,26 @@ from app import create_app
 from werkzeug.security import generate_password_hash
 from ..models import db, Users, Settings
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def app():
+    # Create a new app copy using the test configuration
     app = create_app('app.tests.config.Config')
 
     with app.app_context():
-        db.create_all()
 
-        password="admin"
-        hashed_password=generate_password_hash(password,method="pbkdf2:sha256")
-        user = Users(username="admin",password=hashed_password,firstname="Firstname",lastname="Lastname",email="test@test.com")
-        db.session.add(user)
-        db.session.commit()
-
-        timezone = 'Australia/Perth'
-        db.session.add(Settings(setting="timezone", value=timezone))
-
+        # Do testing
         yield app
 
+@pytest.fixture(scope="function", autouse=True)
+def database(app):
+     with app.app_context():
+        db.create_all()
+        populate_test_database()
+
+        yield app
+        
         db.session.remove()
         db.drop_all()
-
 
 @pytest.fixture()
 def client(app):
@@ -33,3 +32,16 @@ def client(app):
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
+
+def populate_test_database():
+        
+        # Create an admin user
+        password="admin"
+        hashed_password=generate_password_hash(password,method="pbkdf2:sha256")
+        user = Users(username="admin",password=hashed_password,firstname="Firstname",lastname="Lastname",email="test@test.com")
+        db.session.add(user)
+        db.session.commit()
+
+        # Add a timezone
+        timezone = 'Australia/Perth'
+        db.session.add(Settings(setting="timezone", value=timezone))
