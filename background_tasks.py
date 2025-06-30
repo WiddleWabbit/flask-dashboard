823202753ad038d0b646df0861fd0ae64282488a
Nathan Wheat <nwheat@westnet.com.au>
2025-06-30 20:37:57 +0800

    Moved the logger into the app for the background tasks.

--- background_tasks.py
+++ background_tasks.py
@@ -7,6 +7,17 @@ from app.mqtt.mqtt_handler import MQTTHandler
 from app.background_tasks.scheduler import init_scheduler, start_scheduler, shutdown_scheduler, update_mqtt_topics, get_forecast
 from run import app
 import atexit
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level to DEBUG to capture all messages
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Optional: Define log message format
+    handlers=[
+        logging.FileHandler("background_tasks.log"),  # Output logs to a file named 'example.log'
+        logging.StreamHandler() # output to console
+    ]
+)
 
 app = create_app('app.config.Config')
 
@@ -19,13 +30,14 @@ with app.app_context():
 
     # Initialise the MQTT Handler
     app.mqtt_handler = MQTTHandler()
+    app.logger = logging.getLogger(__name__)
 
     # Initialize and start the scheduler
     init_scheduler(app)
-    start_scheduler()
+    start_scheduler(app)
 
     # Register scheduler shutdown when app exits
-    atexit.register(shutdown_scheduler)
+    atexit.register(shutdown_scheduler, app)
 
     # Keep the process alive
     import time
